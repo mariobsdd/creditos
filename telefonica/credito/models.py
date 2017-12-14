c987fccf3e92ed4a60f388a68b2f9d4206d2bb85
Mario Barrientos <mario_b_a03@hotmail.com>
2017-12-14 08:40:41 -0600

    Phone validators in models

--- telefonica/credito/models.py
+++ telefonica/credito/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+from django.core.validators import *
 
 # Create your models here.
 class Cliente(models.Model):
@@ -10,10 +11,10 @@ class Cliente(models.Model):
 	
 	cedula = models.CharField(max_length=50, verbose_name="Número de Documento")
 	tipo = models.CharField(max_length = 50, verbose_name = "Tipo de Documento", choices = DOC_TYPE_CHOICES, default="cedula_identidad")
-	salario = models.FloatField()
-	tel1 = models.IntegerField(verbose_name = "Número de Teléfono 1")
-	tel2 = models.IntegerField(verbose_name = "Número de Teléfono 2")
-	tel3 = models.IntegerField(verbose_name = "Número de Teléfono 3")
+	salario = models.FloatField(validators = [MinValueValidator(0.0)])
+	tel1 = models.CharField(verbose_name = "Número de Teléfono 1", max_length=8, validators=[RegexValidator(r'^\d{1,8}$')])
+	tel2 = models.CharField(verbose_name="Número de Teléfono 2", max_length=8, validators=[RegexValidator(r'^\d{1,8}$')])
+	tel3 = models.CharField(verbose_name = "Número de Teléfono 3", max_length=8, validators=[RegexValidator(r'^\d{1,8}$')])
 	buro = models.BooleanField(choices=BOOL_CHOICES)
 	telefonica = models.BooleanField(choices=BOOL_CHOICES)
 
